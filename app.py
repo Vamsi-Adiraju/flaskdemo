7d051be0d64d9d8469f399c77f787d22e30124f5
Vamsi Adiraju <91866331+Vamsi-Adiraju@users.noreply.github.com>
2022-11-01 10:03:12 +0000

    Added Jobs Dynamically

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
 from flask import Flask,render_template
 
 app = Flask(__name__)
+JOBS=[{
+  'id':1,
+  'title':'Data Analyst',
+  'location':'Bangalore, India',
+  'salary' :'Rs. 10,00,000'
+}, 
+{
+  'id':2,
+  'title':'Data Scientist',
+  'location':'Hyderabad, India',
+  'salary' :'Rs. 15,00,000'
+},
+{
+  'id':1,
+  'title':'Business Analyst',
+  'location':'Pune, India',
+  'salary' :'Rs. 8,00,000'
+},
+{
+  'id':1,
+  'title':'Full Stack Developer',
+  'location':'San Fansisco, USA',
+  'salary' :'$170,000'
+}
+ ]
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",jobs=JOBS)
 
 if __name__== "__main__":
   app.run(host='0.0.0.0',debug=True)
